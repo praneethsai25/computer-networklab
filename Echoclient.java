0e816e2fd4e135a4464b712f9754175098c9999b
praneethsai25 <125631613+praneethsai25@users.noreply.github.com>
2023-02-17 10:40:20 +0530

    Add files via upload

---
+++ Echoclient.java
@@ -0,0 +1,14 @@
+import java.net.socket
+
+public class EchoClient{
+    public static void main(string[]args)
+    try
+    {
+        system.out.println("client started")
+        socket soc- new socket("localhost",9806);
+      catch(Execption e)
+        {
+            e.printstacktrace();
+}
+    }
+}
